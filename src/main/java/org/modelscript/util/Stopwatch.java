1c2b65a8cf05b4ecca1ac29011141b7d898a5c06
vmzakharov <zakharov.vladimir.m@gmail.com>
2020-08-15 21:17:37 -0400

    adds test data generator profile

---
+++ src/main/java/org/modelscript/util/Stopwatch.java
@@ -0,0 +1,41 @@
+package recon.util;
+
+public class Stopwatch
+{
+    private long startTimeMillis;
+    private long endTimeMillis;
+
+    private long startUsedMemoryBytes;
+    private long endUsedMemoryBytes;
+
+    public void start()
+    {
+        this.startUsedMemoryBytes = this.usedMemoryBytes();
+        this.startTimeMillis = System.currentTimeMillis();
+    }
+
+    public void stop()
+    {
+        this.endTimeMillis = System.currentTimeMillis();
+        this.endUsedMemoryBytes = this.usedMemoryBytes();
+    }
+
+    public long elapsedTimeMillis()
+    {
+        return this.endTimeMillis - this.startTimeMillis;
+    }
+
+    public long usedMemoryChangeBytes()
+    {
+        return this.endUsedMemoryBytes - this.startUsedMemoryBytes;
+    }
+
+    private long usedMemoryBytes()
+    {
+        System.gc();
+        System.gc();
+        System.gc();
+        Runtime runtime = Runtime.getRuntime();
+        return runtime.totalMemory() - runtime.freeMemory();
+    }
+}
