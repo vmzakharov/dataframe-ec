4875ec47ba5b9d0f16a5d968c90933f76189f7d5
vmzakharov <zakharov.vladimir.m@gmail.com>
2020-08-29 23:52:54 -0400

    supports scripts rather than just standalone expressions in calculated columns

--- src/main/java/io/github/vmzakharov/ecdataframe/dataframe/DfStringColumnComputed.java
+++ src/main/java/io/github/vmzakharov/ecdataframe/dataframe/DfStringColumnComputed.java
@@ -20,7 +20,7 @@ implements DfColumnComputed
     {
         super(newDataFrame, newName);
         this.expressionAsString = newExpressionAsString;
-        this.expression = ExpressionParserHelper.toExpression(expressionAsString);
+        this.expression = ExpressionParserHelper.toExpressionOrScript(expressionAsString);
     }
 
     @Override
