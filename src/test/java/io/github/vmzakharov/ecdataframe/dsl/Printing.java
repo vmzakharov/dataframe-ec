87e09fabdd623ddde9a82afbb84144236d99d3f2
vmzakharov <zakharov.vladimir.m@gmail.com>
2020-09-27 15:25:42 -0400

    test code clean up

--- src/test/java/io/github/vmzakharov/ecdataframe/dsl/Printing.java
+++ src/test/java/io/github/vmzakharov/ecdataframe/dsl/Printing.java
@@ -17,8 +17,13 @@ public class Printing
                 "3+1 +2";
         AnonymousScript script = ExpressionParserHelper.toScript(scriptText);
 
-        PrettyPrintVisitor visitor = new PrettyPrintVisitor();
-        script.accept(visitor);
+        String result = PrettyPrintVisitor.exprToString(script);
+
+        Assert.assertEquals("x = 1\n" +
+                "y = 2\n" +
+                "z = (x + y)\n" +
+                "((3 + 1) + 2)\n"
+                , result);
     }
 
     @Test
@@ -31,8 +36,13 @@ public class Printing
                 "3 + bar(1, 2) - zzz()";
         AnonymousScript script = ExpressionParserHelper.toScript(scriptText);
 
-        PrettyPrintVisitor visitor = new PrettyPrintVisitor();
-        script.accept(visitor);
+        String result = PrettyPrintVisitor.exprToString(script);
+
+        Assert.assertEquals("x = 1\n" +
+                "y = 2\n" +
+                "foo(x, (y + 2), time(), \"Oy\")\n" +
+                "((3 + bar(1, 2)) - zzz())\n"
+                , result);
     }
 
     @Test
@@ -44,8 +54,9 @@ public class Printing
                 "Foo.qux > 7";
         AnonymousScript script = ExpressionParserHelper.toScript(scriptText);
 
-        PrettyPrintVisitor visitor = new PrettyPrintVisitor();
-        script.accept(visitor);
+        String result = PrettyPrintVisitor.exprToString(script);
+
+        Assert.assertEquals("project {Foo.bar, Foo.baz} where (Foo.qux > 7)\n", result);
     }
 
     @Test
@@ -55,7 +66,6 @@ public class Printing
         Expression expression = ExpressionParserHelper.toExpression(expressionAsString);
 
         String result = PrettyPrintVisitor.exprToString(expression);
-        System.out.println(result);
 
         Assert.assertEquals("((1 + 2) / (7.0 + 9))", result);
     }
