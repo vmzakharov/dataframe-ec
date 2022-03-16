e7ded40d40f02a18d68b0602f6a7510b84e05f79
vmzakharov <zakharov.vladimir.m@gmail.com>
2022-03-16 14:34:03 -0400

    adds Const() aggregation function

--- src/main/java/io/github/vmzakharov/ecdataframe/dataframe/AggregateFunction.java
+++ src/main/java/io/github/vmzakharov/ecdataframe/dataframe/AggregateFunction.java
@@ -587,5 +587,143 @@ public abstract class AggregateFunction
             return 0.0;
         }
     }
+
+    public static class Const
+    extends AggregateFunction
+    {
+        private static final long INITIAL_VALUE_LONG = System.nanoTime();
+        private static final double INITIAL_VALUE_DOUBLE = INITIAL_VALUE_LONG;
+        private static final Object INITIAL_VALUE_OBJECT = new Object();
+
+        public Const(String newColumnName)
+        {
+            super(newColumnName);
+        }
+
+        public Const(String newColumnName, String newTargetColumnName)
+        {
+            super(newColumnName, newTargetColumnName);
+        }
+
+        @Override
+        long longInitialValue()
+        {
+            return INITIAL_VALUE_LONG;
+        }
+
+        @Override
+        double doubleInitialValue()
+        {
+            return INITIAL_VALUE_DOUBLE;
+        }
+
+        @Override
+        Object objectInitialValue()
+        {
+            return INITIAL_VALUE_OBJECT;
+        }
+
+        @Override
+        public boolean handlesObjectIterables()
+        {
+            return true;
+        }
+
+        @Override
+        public void aggregateValueIntoLong(DfLongColumnStored targetColumn, int targetRowIndex, DfColumn sourceColumn, int sourceRowIndex)
+        {
+            long currentAggregatedValue = targetColumn.getLong(targetRowIndex);
+            long nextValue = this.getLongValue(sourceColumn, sourceRowIndex);
+
+            if (currentAggregatedValue == INITIAL_VALUE_LONG)
+            {
+                targetColumn.setObject(targetRowIndex, nextValue);
+            }
+            else if (currentAggregatedValue != nextValue)
+            {
+                targetColumn.setObject(targetRowIndex, null);
+            }
+        }
+
+        @Override
+        public void aggregateValueIntoDouble(DfDoubleColumnStored targetColumn, int targetRowIndex, DfColumn sourceColumn, int sourceRowIndex)
+        {
+            double currentAggregatedValue = targetColumn.getDouble(targetRowIndex);
+            double nextValue = this.getDoubleValue(sourceColumn, sourceRowIndex);
+
+            if (currentAggregatedValue == INITIAL_VALUE_DOUBLE)
+            {
+                targetColumn.setObject(targetRowIndex, nextValue);
+            }
+            else if (currentAggregatedValue != nextValue)
+            {
+                targetColumn.setObject(targetRowIndex, null);
+            }
+        }
+
+        @Override
+        protected Object objectAccumulator(Object currentAggregate, Object newValue)
+        {
+            if (currentAggregate == INITIAL_VALUE_OBJECT)
+            {
+                return newValue;
+            }
+
+            if (currentAggregate != null && currentAggregate.equals(newValue))
+            {
+                return currentAggregate;
+            }
+
+            return null;
+        }
+
+        @Override
+        public Object applyToDoubleColumn(DfDoubleColumn doubleColumn)
+        {
+            double first = doubleColumn.getDouble(0);
+            if (doubleColumn.toDoubleList().allSatisfy(each -> each == first))
+            {
+                return first;
+            }
+
+            return null;
+        }
+
+        @Override
+        public Object applyToLongColumn(DfLongColumn longColumn)
+        {
+            long first = longColumn.getLong(0);
+            if (longColumn.toLongList().allSatisfy(each -> each == first))
+            {
+                return first;
+            }
+
+            return null;
+        }
+
+        @Override
+        public Object applyIterable(ListIterable<?> items)
+        {
+            Object first = items.getFirst();
+            if (items.allSatisfy(each -> each.equals(first)))
+            {
+                return first;
+            }
+
+            return null;
+        }
+
+        @Override
+        public String getDescription()
+        {
+            return "Const";
+        }
+
+        @Override
+        public ValueType targetColumnType(ValueType sourceColumnType)
+        {
+            return sourceColumnType;
+        }
+    }
 }
 
--- src/test/java/io/github/vmzakharov/ecdataframe/dataframe/DataFrameAggregationConstTest.java
+++ src/test/java/io/github/vmzakharov/ecdataframe/dataframe/DataFrameAggregationConstTest.java
@@ -1,12 +1,12 @@
 package io.github.vmzakharov.ecdataframe.dataframe;
 
-import io.github.vmzakharov.ecdataframe.dsl.value.ValueType;
-import org.eclipse.collections.api.list.ListIterable;
 import org.eclipse.collections.impl.factory.Lists;
 import org.junit.Before;
 import org.junit.Test;
 
 import java.time.LocalDate;
+
+import static io.github.vmzakharov.ecdataframe.dataframe.AggregateFunction.Const;
 
 public class DataFrameAggregationConstTest
 {
@@ -44,12 +44,12 @@ public class DataFrameAggregationConstTest
     {
         DataFrameUtil.assertEquals(
                 new DataFrame("aggregated")
-                        .addStringColumn("Name").addStringColumn("Color").addDateColumn("Date")
+                        .addStringColumn("Name").addStringColumn("Same Color").addDateColumn("Date")
                         .addRow("Alice", "Blue",   LocalDate.of(2021, 11, 21))
                         .addRow("Bob",   "Orange", null)
                         .addRow("Carol", null,     LocalDate.of(2021, 12, 22)),
                 this.dataFrame.aggregateBy(
-                        Lists.immutable.of(new Const("Color"), new Const("Date")),
+                        Lists.immutable.of(new Const("Color", "Same Color"), new Const("Date")),
                         Lists.immutable.of("Name")));
     }
 
@@ -73,138 +73,5 @@ public class DataFrameAggregationConstTest
                         new Const("Qux"), new Const("Waldo"), new Const("DateOne"), new Const("DateTwo"))
                 )
         );
-    }
-
-    private static class Const
-    extends AggregateFunction
-    {
-        private static final long INITIAL_VALUE_LONG = System.nanoTime();
-        private static final double INITIAL_VALUE_DOUBLE = INITIAL_VALUE_LONG;
-        private static final Object INITIAL_VALUE_OBJECT = new Object();
-
-        public Const(String newColumnName)
-        {
-            super(newColumnName);
-        }
-
-        @Override
-        long longInitialValue()
-        {
-            return INITIAL_VALUE_LONG;
-        }
-
-        @Override
-        double doubleInitialValue()
-        {
-            return INITIAL_VALUE_DOUBLE;
-        }
-
-        @Override
-        Object objectInitialValue()
-        {
-            return INITIAL_VALUE_OBJECT;
-        }
-
-        @Override
-        public boolean handlesObjectIterables()
-        {
-            return true;
-        }
-
-        @Override
-        public void aggregateValueIntoLong(DfLongColumnStored targetColumn, int targetRowIndex, DfColumn sourceColumn, int sourceRowIndex)
-        {
-            long currentAggregatedValue = targetColumn.getLong(targetRowIndex);
-            long nextValue = this.getLongValue(sourceColumn, sourceRowIndex);
-
-            if (currentAggregatedValue == INITIAL_VALUE_LONG)
-            {
-                targetColumn.setObject(targetRowIndex, nextValue);
-            }
-            else if (currentAggregatedValue != nextValue)
-            {
-                targetColumn.setObject(targetRowIndex, null);
-            }
-        }
-
-        @Override
-        public void aggregateValueIntoDouble(DfDoubleColumnStored targetColumn, int targetRowIndex, DfColumn sourceColumn, int sourceRowIndex)
-        {
-            double currentAggregatedValue = targetColumn.getDouble(targetRowIndex);
-            double nextValue = this.getDoubleValue(sourceColumn, sourceRowIndex);
-
-            if (currentAggregatedValue == INITIAL_VALUE_DOUBLE)
-            {
-                targetColumn.setObject(targetRowIndex, nextValue);
-            }
-            else if (currentAggregatedValue != nextValue)
-            {
-                targetColumn.setObject(targetRowIndex, null);
-            }
-        }
-
-        @Override
-        protected Object objectAccumulator(Object currentAggregate, Object newValue)
-        {
-            if (currentAggregate == INITIAL_VALUE_OBJECT)
-            {
-                return newValue;
-            }
-
-            if (currentAggregate != null && currentAggregate.equals(newValue))
-            {
-                return currentAggregate;
-            }
-
-            return null;
-        }
-
-        @Override
-        public Object applyToDoubleColumn(DfDoubleColumn doubleColumn)
-        {
-            double first = doubleColumn.getDouble(0);
-            if (doubleColumn.toDoubleList().allSatisfy(each -> each == first))
-            {
-                return first;
-            }
-
-            return null;
-        }
-
-        @Override
-        public Object applyToLongColumn(DfLongColumn longColumn)
-        {
-            long first = longColumn.getLong(0);
-            if (longColumn.toLongList().allSatisfy(each -> each == first))
-            {
-                return first;
-            }
-
-            return null;
-        }
-
-        @Override
-        public Object applyIterable(ListIterable<?> items)
-        {
-            Object first = items.getFirst();
-            if (items.allSatisfy(each -> each.equals(first)))
-            {
-                return first;
-            }
-
-            return null;
-        }
-
-        @Override
-        public String getDescription()
-        {
-            return "Const";
-        }
-
-        @Override
-        public ValueType targetColumnType(ValueType sourceColumnType)
-        {
-            return sourceColumnType;
-        }
     }
 }
